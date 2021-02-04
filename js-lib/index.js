e9118393217a1cfda59b429b8a82dd5bc2fe4d07
SfTd <j.zolcik@allblue.pl>
2021-02-04 10:06:31 +0100

    Development.

--- js-lib/index.js
+++ js-lib/index.js
@@ -13,16 +13,17 @@ class abText_Class
         this._langAlias = null;
     }
 
-    $(text)
+    $(text, args = null)
     {
-        js0.args(arguments, 'string');
+        js0.args(arguments, 'string', [ js0.Null, js0.RawObject, 
+                js0.Default ]);
 
         if (this._langAlias === null) {
             console.warn('Lang alias not set.');
-            return this.get(this._langAlias, text);
+            return this.get(this._langAlias, text, args);
         }
 
-        return this.get(this._langAlias, text);
+        return this.get(this._langAlias, text, args);
     }
 
     add(langAlias, texts)
@@ -36,15 +37,34 @@ class abText_Class
         return this;
     }
 
-    get(langAlias, text)
+    get(langAlias, text, args = null)
     {
-        if (!(langAlias in this._texts))
-            return `#${text}#`;
+        js0.args(arguments, [ js0.Null, 'string' ], 'string', [ js0.Null, 
+                js0.RawObject, js0.Default ]);
 
-        if (!(text in this._texts[langAlias]))
-            return `###${text}###`;
+        let translation = null;
 
-        return this._texts[langAlias][text];
+        if (langAlias in this._texts) {
+            if (text in this._texts[langAlias])
+                translation = this._texts[langAlias][text];
+        }
+
+        if (translation === null) {
+            if (args === null)
+                return `#${text}#`;
+            else {
+                let args_Arr = [];
+                for (let argName in args) {
+                    args_Arr.push(`${argName} => ${args[argName]}`);
+                }
+                return '#' + text + '(' + args_Arr.join(', ') + ')';
+            }
+        }
+
+        for (let argName in args)
+            translation.replace(new RegExp(`{${argName}}`, 'gm'), args[argName]);
+
+        return translation;
     }
 
     setLang(langAlias)
